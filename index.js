87d033f251e03bec91134d77851405c472c31ffa
SAW AIK HONG <sawaikhong20@gmail.com>
2021-06-25 16:55:36 +0800

    my api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+//const express = require("express");
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
